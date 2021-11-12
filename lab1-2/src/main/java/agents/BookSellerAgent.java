06f0b1822ab1d72695c56ab31d947df78a2810f1
Alexey Shashko <alex.shashko00@gmail.com>
2021-11-12 21:52:08 +0200

    Lab 1-2

---
+++ lab1-2/src/main/java/agents/BookSellerAgent.java
@@ -0,0 +1,139 @@
+package agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+import java.util.*;
+
+public class BookSellerAgent extends Agent {
+    private Hashtable catalogue;
+    private BookSellerGui myGui;
+
+    protected void setup() {
+        catalogue = new Hashtable();
+
+        myGui = new BookSellerGui(this);
+        myGui.show();
+
+        // Register the book-selling service in the yellow pages
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-selling");
+        sd.setName("JADE-book-trading");
+
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        addBehaviour(new OfferRequestsServer());
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    protected void takeDown() {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        myGui.dispose();
+
+        System.out.println("Seller-agent " + getAID().getName() + " terminating.");
+    }
+
+    /**
+     This is invoked by the GUI when the user adds a new book for sale
+     */
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                catalogue.put(title, new Integer(price));
+                System.out.println(title + " inserted into catalogue. Price = " + price);
+            }
+        });
+    }
+
+    /**
+     Inner class OfferRequestsServer.
+     This is the behaviour used by Book-seller agents to serve incoming requests
+     for offer from buyer agents.
+     If the requested book is in the local catalogue the seller agent replies
+     with a PROPOSE message specifying the price. Otherwise a REFUSE message is
+     sent back.
+     */
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+
+            ACLMessage msg = myAgent.receive(mt);
+
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = (Integer) catalogue.get(title);
+
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                } else {
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+
+    /**
+     Inner class PurchaseOrdersServer.
+     This is the behaviour used by Book-seller agents to serve incoming
+     offer acceptances (i.e. purchase orders) from buyer agents.
+     The seller agent removes the purchased book from its catalogue
+     and replies with an INFORM message to notify the buyer that the
+     purchase has been successfully completed.
+     */
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+
+            ACLMessage msg = myAgent.receive(mt);
+
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = (Integer) catalogue.remove(title);
+
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title + " sold to agent " + msg.getSender().getName());
+                } else {
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    reply.setContent("not-available");
+                }
+
+                myAgent.send(reply);
+
+            } else {
+                block();
+            }
+        }
+    }
+}
